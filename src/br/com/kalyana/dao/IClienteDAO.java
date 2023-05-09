f86d0cd5156ad9964c1d9c9262617addbb227fe7
KalyanaGreim <kalygreim11@gmail.com>
2023-05-08 23:31:19 -0300

    Projeto Modulo 1

---
+++ src/br/com/kalyana/dao/IClienteDAO.java
@@ -0,0 +1,17 @@
+package br.com.kalyana.dao;
+
+import br.com.kalyana.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
